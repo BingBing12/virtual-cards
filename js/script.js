db4054cc7c8e6bc813002dc317dffb0f0b194f1b
BingBing12 <robintainton0509@gmail.com>
2021-11-16 14:22:13 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var menu = document.querySelector(".menu")
+var drop = document.querySelector(".dropdown")
+
+menu.addEventListener("click", function(){
+    if(!drop.classList.contains('active')){
+        drop.classList.add('active')
+    }else{
+        drop.classList.remove('active')
+    }
+    
+});
+
+var card = document.querySelector(".card")
+card.addEventListener("click", function(){
+    if(!card.classList.contains('active')){
+        card.classList.add('active')
+    }else{
+        card.classList.remove('active')
+    }
+})
