09777ae47fb30a820519accc9524ba9d4b51ea7b
BingBing12 <robintainton0509@gmail.com>
2021-11-17 00:22:05 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var menu = document.querySelector(".menu")
 var drop = document.querySelector(".dropdown")
 
+
 menu.addEventListener("click", function(){
     if(!drop.classList.contains('active')){
         drop.classList.add('active')
@@ -11,10 +12,23 @@ menu.addEventListener("click", function(){
 });
 
 var card = document.querySelector(".card")
-card.addEventListener("click", function(){
+/*card.addEventListener("click", function(){
     if(!card.classList.contains('active')){
         card.classList.add('active')
     }else{
         card.classList.remove('active')
     }
-})
+});*/
+
+//scroll trigger animation
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('active');
+        return;
+      }
+      card.classList.remove('active');
+    });
+  });
+  
+  observer.observe(document.querySelector('.card'));
